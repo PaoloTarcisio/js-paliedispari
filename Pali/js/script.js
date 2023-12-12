13d631652053a49a3535dd2b4bd2253a1e4d0bfa
PaoloTarcisio <paoloc50@gmail.com>
2023-12-12 16:29:40 +0100

    impostzione esercizio pali

---
+++ Pali/js/script.js
@@ -0,0 +1,12 @@
+const bottone = document.getElementById('my-button');
+const parola = document.getElementById('parola');
+const lunghezza = parola.length;
+let alorap = '';
+
+bottone.addEventListener('click', function() {
+
+    for (let i = 0; i < 10; i++) {
+        console.log('lunghezza');
+    }
+
+});
